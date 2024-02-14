18158cc1966ae9ca519c73aeea8fb32a2811bc61
NikoBoi12 <150070653+NikoBoi12@users.noreply.github.com>
2024-02-13 20:20:06 -0600

    Create Lerp1.lua

---
+++ ProjectSouls/RandomSystems/LerpCode/Lerp1.lua
@@ -0,0 +1,24 @@
+local WayPoints = workspace.WayPoints -- Just a folder of waypoints
+
+local Orgin = WayPoints.A -- The starting point
+local EndGoal = WayPoints.B -- The end goal
+
+local Target = workspace.Point -- The Non Transparent Part
+
+local Alpha = 0
+
+task.wait(.5)
+
+while true do
+    local DeltaTime = task.wait() -- 1/60th
+    
+    Alpha += .01 -- Alpha is a point inbetween two vectors you can look at it like a percent of the distance between them
+
+    local NewCFrame = Orgin.CFrame:Lerp(EndGoal.CFrame, Alpha) -- Lerps the CFrame for example if you give the vectors (0,0,0) and (6,6,6) and a alpha of .5 it would return (3,3,3)
+
+    Target.CFrame = NewCFrame -- Sets the CFrame
+
+    if Alpha >= 1 then -- Once Alpha is at 1 breaks the loop
+        break
+    end
+end
